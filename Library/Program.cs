5c15ce3bd32ca5ecaf71467a44dadb35e85f5cdb
akthertamanna <tamannaakther10000@gmail.com>
2024-10-08 11:48:46 +0100

    Updated code with a new Member class.

--- Library/Program.cs
+++ Library/Program.cs
@@ -3,7 +3,6 @@
 class Program
 {
     static void Main(string[] args)
-
     {
         // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
